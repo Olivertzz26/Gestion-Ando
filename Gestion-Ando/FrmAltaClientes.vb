9869ffb2b9bfafa7704c91df29dc4821c6505b09
Olivertzz26 <olisebasgoma@gmail.com>
2025-02-18 08:23:32 -0600

    nooooooo xd

--- Gestion-Ando/FrmAltaClientes.vb
+++ Gestion-Ando/FrmAltaClientes.vb
@@ -9,7 +9,7 @@
 
             ' Verifica si el control es un ComboBox
             If TypeOf ctrl Is ComboBox Then
-                ' Borra la selección del ComboBox noooooooooooooooooooooooo
+                ' Borra la selección del ComboBox nooooooooooooooooooooooo
                 DirectCast(ctrl, ComboBox).SelectedIndex = -1
             End If
             TXTCODIGO.Focus()
